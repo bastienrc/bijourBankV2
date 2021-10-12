83db7f0a4cfb3e85106f902a4439a4414e73a924
Sebastien Colbert <colbertsebastien@gmail.com>
2021-10-12 16:38:30 +0200

    Gestion de l’affichage des opérations

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -5,3 +5,67 @@ console.log("Bijour Bank !");
 $(document).ready(function () {
   $(document).foundation();
 });
+
+// Data Test
+const operationsData = [
+  {
+    img: "./assets/images/sac-dargent.png",
+    alt: "credit",
+    title: "Rente",
+    subtitle: "mois de septembre",
+    total: 2879,
+    isCredit: true
+  },
+  {
+    img: "./assets/images/depenses.png",
+    alt: "depense",
+    title: "Friterie",
+    subtitle: "mois de septembre",
+    total: 500,
+    isCredit: false
+  },
+  {
+    img: "./assets/images/sac-dargent.png",
+    alt: "credit",
+    title: "Vinted",
+    subtitle: "mois de septembre",
+    total: 2000,
+    isCredit: true
+  }
+]
+
+// Affichage des opérations
+const listOperations = document.getElementById("listOperations")
+function operationTemplate(operation) {
+  return `
+    <div class="operation ${operation.isCredit ? "credit" : "debit"}">
+      <div class="grid-x grid-padding-x align-middle">
+        <div class="cell shrink">
+          <div class="picto">
+            <img src="${operation.img}" alt="${operation.alt}" />
+          </div>
+        </div>
+        <div class="cell auto">
+          <div>
+            <h2>${operation.title}</h2>
+            <small>${operation.subtitle}</small>
+          </div>
+        </div>
+        <div class="cell small-3 text-right">
+          <div>
+            <p class="count">${operation.total}</p>
+            <small>800%</small>
+          </div>
+        </div>
+      </div>
+    </div>
+  `
+}
+
+listOperations.innerHTML = ""
+operationsData.forEach(op => {
+  listOperations.innerHTML += operationTemplate(op)
+});
+
+
+
